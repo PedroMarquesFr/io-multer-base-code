d14973928471c870016ea3a43e7735e7e2003a69
Pedro Marques Freire <pedromarques1011@gmail.com>
2021-03-16 20:00:57 -0300

    jzuz

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const multer = require("multer");
+const path = require('path');
 
 const app = express();
+
+app.use('/images', express.static(path.join(__dirname, 'uploads')));
 
 const storage = multer.diskStorage({
   destination: (res, file, callback) => {
@@ -16,9 +19,9 @@ const upload = multer({ storage });
 
 app.get("/ping", (req, res) => res.send("pong!"));
 // o nome do arquivo eh importante pra poder identificar de onde ele ta vindo
-app.post("/uploads", upload.single("arquivoNomeFoda"), (req, res) => {
-  console.log(req.body, req.file);
-  res.send("ok!");
+app.post("/uploads", upload.array("arquivoNomeFoda",3), (req, res) => {
+  console.log(req.body, req.files);
+  res.send(req.files);
 });
 
 module.exports = app;
